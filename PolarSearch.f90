87ea2195c15a931951e8af8019c096a62de88ab4
Peter Bosler <pabosle@sandia.gov>
2016-02-16 09:15:09 -0700

    updated copyright date to correct value.

--- PolarSearch.f90
+++ PolarSearch.f90
@@ -40,7 +40,7 @@ namelist /input/ atmfilename, ocnfilename, &
 
 call cpu_time(programTimerStart)
 
-print *, "Stride Search. Copyright 2015 Sandia Corporation. Under the terms of contract DE-AC04-94AL85000, ", &
+print *, "Stride Search. Copyright 2016 Sandia Corporation. Under the terms of contract DE-AC04-94AL85000, ", &
 		 "there is a non-exclusive license for use of this work by or on behalf of the U.S. Government. ", &
 		 " Export of this program may require a license from the United States Government."
 
