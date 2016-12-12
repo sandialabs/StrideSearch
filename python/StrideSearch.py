8af4eb29fe644300c90054769d6bef5609360e29
pbosler <pabosle@sandia.gov>
2016-12-11 18:28:07 -0700

    fixed east-west sign error.'

--- python/StrideSearch.py
+++ python/StrideSearch.py
@@ -4,7 +4,7 @@ import numpy as np
 
 earthRadius_km = 6371.220
 
-class Event:
+class Event(object):
     def __init__(self, lat, lon, year, month, day, hour, dataIndex = None):
         self.lat = lat
         self.lon = lon
@@ -19,9 +19,6 @@ class Event:
             repr(self.datetime.year), repr(self.datetime.month), repr(self.datetime.day), 
             repr(self.datetime.hour), repr(self.dataIndex))
         
-#
-#   is this class necessary?  Couldn't it just be a list of latlon tuples?
-#
 class SectorList(object):
     def __init__(self, southBnd, northBnd, westBnd, eastBnd, radius_km):
         self.southBnd = southBnd
@@ -33,7 +30,7 @@ class SectorList(object):
     
     def __repr__(self):
         return "SectorList({0}, {1}, {2}, {3}, {4})".format(repr(self.southBnd), repr(self.northBnd), 
-            repr(self.eastBnd), repr(self.westBnd), repr(self.r))
+            repr(self.westBnd), repr(self.eastBnd), repr(self.r))
 
     def buildSectorList(self):
         sector_arc_length = self.r / earthRadius_km
@@ -47,8 +44,10 @@ class SectorList(object):
         self.lonStrideReals = lonStrideDegrees
         sectorCenters = []
         for i in range(nStrips+1):
-            nLons = int(math.ceil((self.westBnd - self.eastBnd) / lonStrideDegrees[i]))
-            lons_i = [self.eastBnd + j * lonStrideDegrees[i] for j in range(nLons)]
+            nLons = int(math.ceil((self.eastBnd - self.westBnd) / lonStrideDegrees[i]))
+            #print "nLons = ", nLons
+            lons_i = [self.westBnd + j * lonStrideDegrees[i] for j in range(nLons)]
+            #print sectorLats[i], lons_i
             sectorCenters.append([sectorLats[i],lons_i])
         self.centers = sectorCenters
         
@@ -66,7 +65,7 @@ class SectorList_LatLonGrid(SectorList):
 #         self.setupSectorsForData(None)
     
     def __repr__(self):
-        return "SectorList({0}, {1}, {2}, {3}, {4}, {5}, {6})".format(repr(self.southBnd), repr(self.northBnd), 
+        return "SectorList_LatLonGrid({0}, {1}, {2}, {3}, {4}, {5}, {6})".format(repr(self.southBnd), repr(self.northBnd), 
             repr(self.eastBnd), repr(self.westBnd), repr(self.r), repr(self.nLat), repr(self.nLon))
 
     def setupSectorsForData(self, null_arg):     
@@ -84,11 +83,12 @@ if __name__ == "__main__":
     ev = Event(45.0, 0.0, 2016, 12, 8, 13)
     print "ev.latlon() = ", ev.latlon()  
     print ev
-    sc_list = SectorList(0.0, 80.0, 180.0, 360.0, 1000.0) 
-    print sc_list
-    print sc_list.centers
+    #sc_list = SectorList(0.0, 80.0, 180.0, 360.0, 1000.0) 
+    #print sc_list
+    #print "Sector centers : ", sc_list.centers
     sc_ll = SectorList_LatLonGrid(0.0, 80.0, 180.0, 360.0, 1000.0, 91, 181) 
     print sc_ll
+    print "Sector centers : ", sc_ll.centers
 
              
     
