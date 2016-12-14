7f4be632d5ef85763161c207325578ae756de5ed
Peter Bosler <pabosle@sandia.gov>
2016-12-14 09:12:12 -0700

    Event unit test updated. IdentCriteria unit test updated. Filled in rest of identification criteria subclasses

--- python/Event.py
+++ python/Event.py
@@ -11,7 +11,7 @@ from datetime import datetime
 def print_copyright():
     print """Stride Search. Copyright 2016 Sandia Corporation. Under the terms of contract DE-AC04-94AL85000, 
 there is a non-exclusive license for use of this work by or on behalf of the U.S. Government. 
-Export of this program may require a license from the United States Government.\n\n"""
+Export of this program may require a license from the United States Government.\n"""
 
 #def earthRadius_km():
 #    """Earth mean sea level radius in kilometers"""
@@ -20,7 +20,8 @@ Export of this program may require a license from the United States Government.\
 class Event(object):
     """
     An Event is a storm or other relevant event to a user of this software.
-    At a minimum, an Event has a date and location that correspond to a data index.
+    At a minimum, an Event has a date and location that correspond to a data index,
+    and a set of key-value pairs (vals) describing the Event.
     """
     def __init__(self, desc, latLon, dtime, dataIndex = None, vals = None):
         self.desc = desc
@@ -30,15 +31,25 @@ class Event(object):
         self.vals = vals
           
     def __repr__(self):
-        return "Event({0}, {1}, {2}, {3}, {4}, {5}, {6}, {7})".format(repr(self.desc), repr(self.lat), repr(self.lon), 
-            repr(self.datetime.year), repr(self.datetime.month), repr(self.datetime.day), 
-            repr(self.datetime.hour), repr(self.dataIndex))
-
-            
+        return "Event({0}, {1}, {2}, {3}, {4})".format(repr(self.desc), repr(self.latLon),
+             repr(self.datetime), repr(self.dataIndex), repr(self.vals))
+     
+    def printData(self):
+        for att in ['desc', 'latLon', 'datetime', 'dataIndex', 'vals']:
+    		print '\t', att, ':', getattr(self, att)
                                     
 if __name__ == "__main__":
     print_copyright()
-    ev = Event('unitTest', (45.0, 0.0), 2016, 12, 8, 13)
-    print "ev.latlon() = ", ev.latlon()  
+    dt = datetime.now()
+    dtin = datetime(dt.year, dt.month, dt.day, dt.hour)
+    desc = 'unitTest'
+    ll = (45.0, 0.0)
+    inds = (1,1)
+    vals = {'testval' : 100.0}
+    ev = Event(desc, ll, dtin, inds, vals)
+    print 'Event created:'
     print ev
+    print 'Event data:'
+    ev.printData()
+    
 
--- python/IdentCriteria.py
+++ python/IdentCriteria.py
@@ -4,22 +4,20 @@ Implementations for basic criteria are included.
 """
 from Event import Event, print_copyright
 from abc import ABCMeta, abstractmethod
-from numpy import amax, amin, ones, mean, argmax, argmin
+from numpy import amax, amin, ones, mean, argmax, argmin, array, linspace, concatenate
 from SectorList import Sector
 from datetime import datetime
 
 class Criterion(object):
-"""
-Each criterion will be evaluated once for each sector, during each time step.
-If an evaluation returns True, then the Criterion can return the corresponding Event.
-"""
+    """
+    Each criterion will be evaluated once for each sector, during each time step.
+    If an evaluation returns True, then the Criterion can return the corresponding Event.
+    """
     __metaclass__ = ABCMeta
     
     def __init__(self, varname, threshold):
         self.varnames = [varname]
         self.threshold = threshold
-        #if crit_type not in criteria_types:
-        #    raise NotImplementedError("Criterion ERROR: requested criteria (%s) not implemented."%crit_type)
     
     def addVariableName(self, varname):
         self.varnames.append(varname)    
@@ -27,30 +25,32 @@ If an evaluation returns True, then the Criterion can return the corresponding E
     def __repr__(self):
         return "<%s: varname = %s, threshold = %s>"%(self.__class__.__name__, self.varnames[0], self.threshold)
     
-    @abstractmethod        
+    @abstractmethod
     def evaluate(self, sector, workspace):
         pass
       
     @abstractmethod
-    def returnEvent(self, data, sector, workspace):
+    def returnEvent(self, sector, workspace, dtime):
         pass
-        
+   
+    def printData(self):
+    	for att in ['varnames', 'threshold']:
+    		print '\t', att, ':', getattr(self, att)
+    
 class MaxCriterion(Criterion):
     """ 
     Maximum value criterion.  Returns True if the maximum value of the data 
     meets or exceeds the threshold
     """
     def evaluate(self, sector, workspace):
-        if amax(workspace[self.varnames[0]], axis=0) >= self.threshold:
-            return True
-        else:
-            return False
+        return amax(workspace[self.varnames[0]], axis=0) >= self.threshold
+
             
     def returnEvent(self, sector, workspace, dtime):
-    	val = amax(workspace[self.varnames[0]])
+        val = amax(workspace[self.varnames[0]])
         ind = argmax(workspace[self.varnames[0]])
-        vals = {varnames[0] : maxval}
-        desc = varnames[0] + " max"
+        vals = {self.varnames[0] : val}
+        desc = self.varnames[0] + " max"
         return Event(desc, sector.dataPoints[ind], dtime, sector.dataPointIndices[ind], vals)
 
                                                         
@@ -60,58 +60,62 @@ class MinCriterion(Criterion):
     or falls below the threshold.
     """
     def evaluate(self, sector, workspace):
-        if amin(data.ravel(), axis=0) < self.threshold:
-            return True
-        else:
-            return False
+        return amin(workspace[self.varnames[0]]) <= self.threshold
+
             
-    def returnEvent(self, sector, workspace):
-        pass
+    def returnEvent(self, sector, workspace, dtime):
+        val = amin(workspace[self.varnames[0]])
+        ind = argmin(workspace[self.varnames[0]])
+        vals = {self.varnames[0] : val}
+        desc = self.varnames[0] + ' min'
+        return Event(desc, sector.dataPoints[ind], dtime, sector.dataPointIndices[ind], vals)
  
 
 class MaxAverageCriterion(Criterion):
-    """
+    """ 
     Maximum average criterion. Returns True if the mean of the data meets or 
     exceeds the threshold.
     
     NOTE: The average computed here is an arithmetic average, not a spatial average.
     """ 
     def evaluate(self, sector, workspace):
-        if mean(data.ravel(), axis=0) >= self.threshold:
-            return True
-        else:
-            return False
-    def returnEvent(self, sector, workspace):
-        pass
-  
+        return mean(workspace[self.varnames[0]]) >= self.threshold
 
+    def returnEvent(self, sector, workspace, dtime):
+        val = amax(mean(workspace[self.varnames[0]]))
+        ind = argmax(mean(workspace[self.varnames[0]]))
+        vals = {self.varnames[0] : val}
+        desc = 'max avg(' + self.varnames[0] + ')'
+        return Event(desc, sector.dataPoints[ind], dtime, sector.dataPointIndices[ind], vals)
+        
 class VariationExcessCriterion(Criterion):
-    """
+    """ 
     Variation excess criterion.  Returns True if the maximum data value exceeds 
     the data average by the threshold value or greater.
     
     NOTE: The average computed here is an arithmetic average, not a spatial average.
-    """                        
+    """
     def evaluate(self, sector, workspace):
-        if amax(data.ravel(), axis=0) - mean(data.ravel(), axis=0) >= self.threshold:
-            return True
-        else:
-            return False
+        return amax(workspace[self.varnames[0]] - mean(workspace[self.varnames[0]])) >= self.threshold
+
     def returnEvent(self, sector, workspace):
-        pass
-        
-   
+        val = amax(workspace[self.varnames[0]] - mean(workspace[self.varnames[0]]))
+        ind = argmax(workspace[self.varnames[0]] - mean(workspace[self.varnames[0]]))
+        vals = {'var(' + self.varnames[0] + ')' : val}
+        desc = 'max var(' + self.varnames[0] + ')'
+        return Event(desc, sector.dataPoints[ind], dtime, sector.dataPointIndices[ind], vals)
 
 class DifferenceCriterion(Criterion):
-    def __init__(self, varname1, varname2, threshold):
-        Criterion.__init__(self, varname1, threshold)
-        self.addVariableName(varname2)
     """
     Difference criterion. Returns True if the maximum element-wise value of the 
     difference data1 - data2 meets or exceeds the threshold.
     """
+    def __init__(self, varname1, varname2, threshold):
+        Criterion.__init__(self, varname1, threshold)
+        self.addVariableName(varname2)
+    
     def evaluate(self, sector, workspace):
-        if amax(data1.ravel() - data2.ravel(), axis=0) >= self.threshold:
+        if amax(workspace[self.varnames[0]] - workspace[self.varnames[1]]) >= self.threshold:
             return True
         else:
             return False
@@ -120,21 +124,46 @@ class DifferenceCriterion(Criterion):
        return "<%s: varname = %s, varname2 = %s, threshold = %s>"%(self.__class__.__name__, 
         self.varnames[0], self.varnames[1], self.threshold)       
     
-    def returnEvent(self, sector, workspace):
-        pass 
-        
-                   
+    def returnEvent(self, sector, workspace, dtime):
+        val = amax(workspace[self.varnames[0]] - workspace[self.varnames[1]])
+        ind = argmax(workspace[self.varnames[0]] - workspace[self.varnames[1]])
+        vals = { 'diff(' + self.varnames[0] + ',' + self.varnames[1] + ')' : val}
+        desc = 'max(' + self.varnames[0] + ' - ' + self.varnames[1] + ')'
+        return Event(desc, sector.dataPoints[ind], dtime, sector.dataPointIndices[ind], vals)         
         
 if __name__ == "__main__":
     print_copyright()
+    # Test constructor, basic fns
     c = MaxCriterion("vorticity", 1.0E-4)
-    vor_data = ones((2,5))
+    print 'MaxCriterion created.'
     print(c)
-    #print "criterion eval: ", c.evaluate(vor_data)
-    aa = 2.0 * ones(4)
-    bb = 6.0 * ones(4)
+    c.printData()
+    # Test evaluate
+    rn = datetime.now()
+    testDt = datetime(rn.year, rn.month, rn.day, rn.hour)
+    secLats = linspace(-6.0, 6.0, 13)
+    secLons = linspace(-6.0, 6.0, 13)
+    secDataPts = [(lat,lon) for lat in secLats for lon in secLons]
+    latInds = linspace(84, 96, 13, dtype = int)
+    lonIndsLeft = linspace(354, 359, 6, dtype = int)
+    lonIndsRight = linspace(0, 6, 7, dtype = int)
+    lonInds = concatenate((lonIndsLeft, lonIndsRight))
+    secInds = [(i, j) for i in latInds for j in lonInds]
+    testSector = Sector(0.0, 0.0, 1000.0, 500.0, 500.0, secDataPts, secInds)
+    maxWorkspace = {'vorticity': linspace(0.0, 1.0, len(secDataPts))}
+    print 'max criterion evaluate result = ', c.evaluate(testSector, maxWorkspace)
+    maxEv = c.returnEvent(testSector, maxWorkspace, testDt)
+    print 'max criterion returned event = ', maxEv
+    maxEv.printData()
+    
+    diffWorkspace = {'a' : linspace(0.0, 50.0, len(secDataPts)), 'b' : linspace(50.0, 100.0, len(secDataPts))}
     cd = DifferenceCriterion("b","a", 1.0)
+    print 'DifferenceCriterion created.'
     print cd
-    #print "criterion eval: ", cd.evaluate(bb,aa)
+    cd.printData()
+    print 'DifferenceCriterion evaluate result = ', cd.evaluate(testSector, diffWorkspace)
+    diffEv = cd.returnEvent(testSector, diffWorkspace, testDt)
+    print 'Difference criterion returned event = ', diffEv
+    diffEv.printData()
         
                         
