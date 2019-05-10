d562b221fe206fcadda87909dc034dc8c9b51ab1
Peter Bosler <pabosle@sandia.gov>
2019-05-10 13:48:03 -0600

    revised workspace.

---
+++ src/SSWorkspace.cpp
@@ -0,0 +1,33 @@
+#include "SSWorkspace.hpp"
+
+namespace StrideSearch {
+
+Workspace::Workspace(const std::string& var, const Index n) {
+    data.emplace(var, RealArray(n,0.0));
+}
+
+Workspace::Workspace(const std::vector<std::string>& vars, const Index n) {
+    for (auto& var : vars)
+        data.emplace(var, RealArray(n,0.0));
+}
+
+const RealArray& Workspace::getConstDataRef(const std::string& var) const {
+    return const_cast<RealArray&>(data.at(var));
+}
+
+RealArray& Workspace::getDataRef(const std::string& var) {
+    return data.at(var);
+}
+
+std::ostream& operator << (std::ostream& os, const Workspace& wspc) {
+    for (auto& elem : wspc.data) {
+        os << elem.first << ": ";
+        for (Index i=0; i<elem.second.size(); ++i) {
+            os << elem.second[i] << " ";
+        }
+        os << std::endl;
+    }
+    return os;
+}
+
+}
---
+++ src/SSWorkspace.hpp
@@ -0,0 +1,52 @@
+#ifndef _SS_WORKSPACE_HPP_
+#define _SS_WORKSPACE_HPP_
+
+#include "StrideSearchConfig.h"
+#include "SSDefs.hpp"
+#include <map>
+#include <vector>
+#include <string>
+#include <iostream>
+
+namespace StrideSearch {
+/// Space for each sector to have its own local memory for the variables associated with a set of IDCriterion subclasses.
+/** A Workspace is a set of dictionaries whose keys are variable names and whose values arrays of data read from 
+    a netCDF file.  
+    
+    Each IDCriterion has its own workspace, but the workspace may contain multiple data variables if the IDCriterion
+    requires.
+    
+*/
+struct Workspace {
+    /// A workspace simply holds scalar data values
+    /**
+        The container is a std::map; key = variable name, value = vector of data values
+    */
+    std::map<std::string, RealArray> data;
+    
+    /// Default constructor, creates null workspace.
+    Workspace() : data() {}
+    
+    /// Constructor with memory allocation
+    Workspace(const std::string& var, const Index n);
+    
+    /// Constructor with memory allocation
+    Workspace(const std::vector<std::string>& vars, const Index n);
+    
+    /// Fill workspace with data from a predefined array.  Used for testing.
+    void fillData(const std::string& var, const RealArray& vals);
+    
+    /// Return a constant (read only) reference to the data stored in one workspace variable.
+    const RealArray& getConstDataRef(const std::string& var) const;
+    
+    /// Return a non-const reference to workspace data 
+    RealArray& getDataRef(const std::string& var);
+    
+    /// Return the number of variables used by *this
+    inline Int nVars() const {return data.size();}
+};
+
+std::ostream& operator << (std::ostream& os, const Workspace& wspc);
+
+}
+#endif
