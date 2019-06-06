e7b26b33b0b1c9d506b7736fb032c083d70611c3
Peter Bosler <pabosle@sandia.gov>
2019-06-06 10:08:01 -0600

    added spatial integral approximation with midpoint rule. added location to average value criteria.

--- src/SSNCReader.cpp
+++ src/SSNCReader.cpp
@@ -109,6 +109,12 @@ void LatLonNCReader::fillWorkspaceData(Workspace& wspc,
                 var.second[i] = lats[inds[i][0]];
             }
         }
+        else if (var.first == "area_weight" || var.first == "weight") {
+            const Real dlam = this->resolutionEstimate()/EARTH_RADIUS_KM;
+            for (Int i=0; i<inds.size(); ++i) {
+                var.second[i] = 2*SQ_EARTH_RADIUS_KM * dlam * std::cos(lats[inds[i][0]])*std::sin(dlam);
+            }
+        }
         else {
             netCDF::NcVar ncv(ncfile->getVar(var.first));
             const Int ndim = ncv.getDimCount();
@@ -156,6 +162,12 @@ void UnstructuredNCReader::fillWorkspaceData(Workspace& wspc,
                 var.second[i] = lats[inds[i][0]];
     	    }
     	}
+    	else if (var.first == "area_weight" || var.first == "weight") {
+            const Real dlam = this->resolutionEstimate()/EARTH_RADIUS_KM;
+            for (Int i=0; i<inds.size(); ++i) {
+                var.second[i] = 2*SQ_EARTH_RADIUS_KM * dlam * std::cos(lats[inds[i][0]])*std::sin(dlam);
+            }
+        }
     	else {
             netCDF::NcVar ncv(ncfile->getVar(var.first));
             const Int ndim = ncv.getDimCount();
